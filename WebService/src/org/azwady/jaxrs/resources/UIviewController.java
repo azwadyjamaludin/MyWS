c4a31f595e61b8c63b21a629334f3dc6dd194c22
Azi Azwady Jamaludin <aziazwady@gmail.com>
2018-09-14 11:35:05 +0800

    slight changes on method to view html

--- WebService/src/org/azwady/jaxrs/resources/UIviewController.java
+++ WebService/src/org/azwady/jaxrs/resources/UIviewController.java
@@ -8,9 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 
 @Path("/uiview")
 public class UIviewController {
@@ -21,9 +19,15 @@ public class UIviewController {
 	
 	@POST
 	@Path("/index")
-	@Produces(MediaType.TEXT_HTML)
-	public void index(@Context HttpServletResponse servletResponse) throws IOException {
-		servletResponse.sendRedirect("../View/index.html");
+	public void index(@Context HttpServletRequest servletRequest,
+					  @Context HttpServletResponse servletResponse) {
+		try {
+            servletRequest.getRequestDispatcher("/View/index.html").forward(servletRequest, servletResponse);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 	}
 	
 	@GET
